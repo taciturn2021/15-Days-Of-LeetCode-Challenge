8cb142bded574392c2510a4ba671ef4515f6307f
taciturn2021 <darkraiden112@gmail.com>
2024-12-06 15:10:00 +0500

    Time: 0 ms (100.00%) | Memory: 3.9 MB (47.19%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.go
@@ -0,0 +1,43 @@
+func rob(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    if len(nums) == 2 {
+        return max(nums[0], nums[1])
+    }
+    return max(HouseRobberOne(nums[:len(nums)-1]), HouseRobberOne(nums[1:]))
+}
+
+
+
+
+
+func HouseRobberOne(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    if len(nums) == 2 {
+        return max(nums[0], nums[1])
+    }
+    prev2 := nums[0]          
+    prev1 := max(nums[0], nums[1])  
+    for i := 2; i < len(nums); i++ {
+        current := max(prev1, prev2 + nums[i])
+        prev2 = prev1
+        prev1 = current
+    }
+    return prev1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
