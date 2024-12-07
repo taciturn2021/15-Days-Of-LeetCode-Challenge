7ff16d446ed2cd82bd17fb2788e2e926ecc4183c
taciturn2021 <darkraiden112@gmail.com>
2024-12-07 15:27:16 +0500

    Time: 5 ms (35.50%) | Memory: 6.3 MB (23.80%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.go
@@ -0,0 +1,16 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+    str := strconv.Itoa(x)
+    start , end := 0 , len(str) - 1
+    for start < end{
+        if str[start] != str[end]{
+            return false
+        }
+        start++
+        end--
+    }
+    return true
+
+}
