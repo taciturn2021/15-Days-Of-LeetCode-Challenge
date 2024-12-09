212c8c2901c37facac011e7cdce10a42808cf840
taciturn2021 <darkraiden112@gmail.com>
2024-12-09 19:20:56 +0500

    Time: 15 ms (54.44%) | Memory: 13.1 MB (19.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,5 +1,5 @@
 func containsDuplicate(nums []int) bool {
-    appears := make(map[int]int)
+    appears := make(map[int]int, len(nums))
 
     for i := 0; i < len(nums); i++{
         appears[nums[i]]++
