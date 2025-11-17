cb7faa2e5234999535bdcffc2b6548b2b8ad31ba
Muhammad Abdullah <taciturn2021@gmail.com>
2025-11-17 15:46:35 +0500

    Time: 36 ms (60.15%) | Memory: 74.6 MB (15.52%) - LeetSync

---
+++ 15-3sum/3sum.ts
@@ -0,0 +1,35 @@
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a,b) => a-b);
+    let answer:number[][] = [];
+    for(let i = 0; i < nums.length - 2; i++){
+        if (i > 0 && nums[i] === nums[i-1]) {
+            continue;
+        }
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            if (nums[i] + nums[left] + nums[right] === 0){
+                answer.push([nums[i],nums[left],nums[right]]);
+                while (left < right && nums[left] === nums[left+1]) {
+					left++
+				}
+				while (left < right && nums[right] === nums[right-1]) {
+					right--
+				}
+				left++
+				right--
+            }
+            else if (nums[i] + nums[left] + nums[right] > 0){
+                right--;
+            }
+            else if (nums[i] + nums[left] + nums[right] < 0){
+                left++;
+            }
+
+
+        }
+
+
+    }
+    return answer
+};
