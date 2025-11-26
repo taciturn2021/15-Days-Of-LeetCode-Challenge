1397faaeafef4c58c7b315e03c560156d090fa10
Muhammad Abdullah <taciturn2021@gmail.com>
2025-11-26 20:10:35 +0500

    Time: 4 ms (2.94%) | Memory: 56.6 MB (5.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -0,0 +1,17 @@
+var memo: number[] = []
+function climbStairs(n: number): number {
+    let memo = []
+    for (let i = 0; i < n+1; i++) {
+        memo[i] = -10;
+    }
+    memo[0] = 1;
+    memo[1] = 1;
+    let total =0;
+    for (let i = 2; i < n+1; i++){
+        memo[i] = memo[i-1] + memo[i-2];
+    }
+    console.log(memo);
+   
+    return memo[memo.length-1];
+    
+    };
