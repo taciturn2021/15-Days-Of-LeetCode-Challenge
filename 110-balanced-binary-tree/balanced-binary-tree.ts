a48dd2728a8a547e8eb90ba7a032bfd6a27fe4a0
Muhammad Abdullah <taciturn2021@gmail.com>
2025-11-27 20:09:24 +0500

    Time: 0 ms (100.00%) | Memory: 60.8 MB (73.22%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.ts
+++ 110-balanced-binary-tree/balanced-binary-tree.ts
@@ -13,44 +13,20 @@
  */
 
 function isBalanced(root: TreeNode | null): boolean {
-    if (root === null) return true;
 
-    var check = [];
-    const runOnEachNode = (node) => {
-        if (node === null) return ;
+    const dfs = (node: TreeNode | null): number => {
+        if (node === null) return 0;
 
-        runOnEachNode(node.left);
-        let result = checkBalanced(node);
-        if (result === false) {
-            check = [false];
-            return;
-        }
-        check.push(result);
-        runOnEachNode(node.right);
-    }
+        const left = dfs(node.left);
+        if (left === -1) return -1;   
 
-    const checkBalanced = (node) => {
+        const right = dfs(node.right);
+        if (right === -1) return -1;  
 
-        let leftDepth = findMaxDepth(node.left,0);
-        let rightDepth = findMaxDepth(node.right,0);
-        let diff = Math.abs(leftDepth - rightDepth);
-        return diff < 2;
-    }
+        if (Math.abs(left - right) > 1) return -1; 
 
+        return Math.max(left, right) + 1;  
+    };
 
-
-    const findMaxDepth =  (node, height) => {
-        if (node === null) return height;
-
-        let heightLeft = findMaxDepth(node.left, height + 1);
-        let heightRight = findMaxDepth(node.right, height + 1);
-        
-        return (heightLeft > heightRight) ? heightLeft: heightRight ;
-    }
-    runOnEachNode(root);
-    console.log(check);
-    check = check.filter(item => item === false);
-    console.log(check);
-    if (check.length > 0 ) return false;
-    return true;
-};
+    return dfs(root) !== -1;
+}
