207d6647d5c6eb8649b0d2f05880a501cca4c5e5
Muhammad Abdullah <taciturn2021@gmail.com>
2026-02-01 16:08:57 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (85.47%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,21 +5,10 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        valCount = 0
-        length = len(nums)
-    
+        j = 0
         for i in range(len(nums)):
-            if nums[i] == val:
-                nums[i] = '_'
-                valCount += 1
-        
-        index = 0
-        while index < len(nums):
-            if nums[index] == '_':
-                nums.pop(index)
-            else:
-                index += 1
-        
-        k = length - valCount
-
-        return k
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1    
+        nums[:] = nums[:j]
+        return j
