a1b731f2498af328274245e792dec02917a58a8d
Muhammad Abdullah <taciturn2021@gmail.com>
2026-02-01 19:16:27 +0500

    Time: 4 ms (65.60%) | Memory: 22.9 MB (22.28%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -6,22 +6,8 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
-        left, right = 0, len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right] , nums[left]
-            left += 1
-            right -= 1
+        nums.reverse()
 
-        
-        left, right = 0, (k - 1)
-        while left < right:
-            nums[left], nums[right] = nums[right] , nums[left]
-            left += 1
-            right -= 1
-        
-        left, right = k , len(nums)-1
-        while left < right:
-            nums[left], nums[right] = nums[right] , nums[left]
-            left += 1
-            right -= 1
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
 
