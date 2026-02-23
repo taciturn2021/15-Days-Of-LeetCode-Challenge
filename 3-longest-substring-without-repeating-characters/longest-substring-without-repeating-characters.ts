ba3d0c1b172543f2003b00d8eaed900da011f80f
Muhammad Abdullah <taciturn2021@gmail.com>
2026-02-23 20:57:41 +0500

    Time: 9 ms (39.05%) | Memory: 60.3 MB (45.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,19 +1,20 @@
 function lengthOfLongestSubstring(s: string): number {
-    let queue: string[] = []
-    let maxLength: number = 0;
-
-   for (let i of s) {
-    if (queue.includes(i)){
-        maxLength = (queue.length > maxLength) ? queue.length : maxLength;
-        while (queue[0] != i) {
-            queue.splice(0 , 1);
-        }
-        queue.splice(0,1);
+    if (s.length == 0 || s.length == 1) {
+        return s.length;
     }
-    queue.push(i);
-   }
+    let queue = []
+    let maxLength = 0
+    for (let i of s){
+        if (queue.includes(i)) {
+            maxLength = maxLength > queue.length ? maxLength: queue.length;
+            while (queue[0] != i){
+                queue.splice(0,1);
+            }
+            queue.splice(0,1);
+        }
 
-   return (maxLength > queue.length) ? maxLength : queue.length;
+        queue.push(i);
+    }
+    maxLength = maxLength > queue.length ? maxLength: queue.length;
+    return maxLength 
 };
-
-
