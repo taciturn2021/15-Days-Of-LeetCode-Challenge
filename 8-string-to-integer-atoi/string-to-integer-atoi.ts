0d9f1e532c5a25772ff8345c49daa2406cd144af
Muhammad Abdullah <taciturn2021@gmail.com>
2026-02-24 21:49:32 +0500

    Time: 64 ms (5.08%) | Memory: 63.4 MB (5.08%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.ts
@@ -0,0 +1,63 @@
+function myAtoi(s: string): number {
+    let chars = []
+    for (let i of s) {
+        chars.push(i);
+    }
+   
+    // remove white spaces from the start
+    let index = 0;
+    while (index < chars.length) {
+        if (chars[index] !== " ") break;
+        chars.splice(index,1);
+        
+    }
+    // remove white spaces from the end;
+    index = 0;
+    while (index >= 0) {
+        if (chars[index] !== " ") break;
+        chars.splice(index,1);
+        index--;
+    }
+    console.log(`white space removal: ${chars}`);
+    // extract sign
+    let sign = "+"
+    if (chars[0] == "+" || chars[0] == "-") {
+        sign = chars[0];
+        chars.shift();
+    }
+    console.log(`Sign: ${sign}`);
+
+    // extract number
+    let num = "";
+    index = 0;
+    let digits = ["0","1","2","3","4","5","6", "7", "8", "9"];
+    while (index < chars.length) {
+        if (digits.includes(chars[index])) {
+            num = num + chars[index];
+            index++;
+        } else {
+            break;
+        }
+    }
+    console.log(`Num before Int Limit ${num}`);
+    let upperMax = 2 ** 31 -1;
+    let lowerMax = 2 ** 31 * -1;
+    let ans = 0
+    if (num.length !== 0 ) {
+        ans = parseInt(num);
+    }
+
+    if(sign === "-") {
+        ans = ans * -1;
+        if (ans < lowerMax) {
+            ans = lowerMax;
+        }
+    } else {
+        if (ans > upperMax) {
+            ans = upperMax;
+        }
+    }
+
+    console.log(`final answer: ${ans}`);
+    return ans;
+};
