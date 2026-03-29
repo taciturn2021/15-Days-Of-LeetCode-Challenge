796a929f0944179397d2fa4411b6596e5bfd621c
Muhammad Abdullah <taciturn2021@gmail.com>
2026-03-29 16:37:42 +0500

    Time: 12 ms (69.16%) | Memory: 57.4 MB (78.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,4 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
-    
-    return JSON.stringify(s.split("").toSorted()) === JSON.stringify(t.split("").toSorted())
+    if (s.length !== t.length) return false;
+    const map1 = new Map<string, number>();
+    const map2 = new Map<string, number>();
+    for(let i = 0; i < s.length; i++){
+        map1.set(s[i], (map1.get(s[i]) || 0) +1);
+        map2.set(t[i], (map2.get(t[i]) || 0) +1);
+    }
+    for (let key of map1.keys()) {
+        if (map1.get(key) !== map2.get(key)) return false;
+    }
+    return true;
 };
